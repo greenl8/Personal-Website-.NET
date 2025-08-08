f055840fe3bdcc06d80fd2223a5f74b343b26a5e
J <jacksonhick330@gmail.com>
2025-08-08 13:11:32 +0100

    Render push 0.19.2.9.4

--- greenl8site/greenl8web-client/src/app/services/greenl8web.service.ts
+++ greenl8site/greenl8web-client/src/app/services/greenl8web.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HelloWorldService {
-  private apiUrl = 'http://localhost:5046/api/test'; // Adjust port as needed for your .NET app
+  private apiUrl = environment.apiUrl + 'test';
 
   constructor(private http: HttpClient) { }
 
